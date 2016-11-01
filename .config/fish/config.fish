db7ab11879b89763902e06a75571948eb1e5eabe
Kohei Asai <me@axross.io>
2016-11-01 16:31:20 +0900

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,12 @@ set fish_color_param cyan
 
 set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+
+set -x PATH /usr/local/opt/dart/libexec $PATH
+
+set -x N_PREFIX $HOME/.n
+set -x PATH $N_PREFIX/bin $PATH
+
+set -x PATH $HOME/.yarn/bin $PATH
+
+set -x DDC_PATH $HOME/Dropbox/Workspace/sdk/pkg/dev_compiler
